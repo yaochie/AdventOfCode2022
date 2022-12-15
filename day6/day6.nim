f062e8ddf0b800fc8df12fc09f2e58a641d49265
yaochie <>
2022-12-16 00:36:56 +0800

    add day 6

---
+++ day6/day6.nim
@@ -0,0 +1,28 @@
+import std/deques
+import std/intsets
+import std/packedsets
+import std/sets
+import std/enumerate
+
+let f = open("day6.txt")
+let text = f.readAll()
+f.close()
+
+proc findUniques(text: string, uniqueCount: int) =
+    var recent: Deque[char] = initDeque[char]()
+    for i, c in enumerate(1, text):
+        recent.addLast(c)
+
+        if recent.len() == uniqueCount:
+            var a: set[char]
+            for b in recent:
+                incl(a, b)
+
+            if a.len() == uniqueCount:
+                echo i
+                return
+
+            recent.popFirst()
+
+findUniques(text, 4)
+findUniques(text, 14)
